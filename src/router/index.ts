2aa36bb1a4c55a3da2101e8c69feec4b0f11dd4c
zacharylutfyr <zacharylutfyr@gmail.com>
2023-05-22 00:40:10 +0700

    "Add Home Component"

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,40 +1,46 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import LoginComponent from '@/components/auth/LoginComponent.vue'
-import RegisterComponent from '@/components/auth/RegisterComponent.vue'
-import ForgotPassword from '@/components/auth/ForgotPassword.vue'
-import NotFound from '@/components/pages/NotFound.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import LoginComponent from "@/components/auth/LoginComponent.vue";
+import RegisterComponent from "@/components/auth/RegisterComponent.vue";
+import ForgotPassword from "@/components/auth/ForgotPassword.vue";
+import NotFound from "@/components/pages/NotFound.vue";
+import HomeComponent from "@/components/pages/HomeComponent.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'LoginComponent',
-    component: LoginComponent
+    path: "/",
+    name: "LoginComponent",
+    component: LoginComponent,
   },
   {
-    path: '/register',
-    name: 'RegisterComponent',
-    component: RegisterComponent
+    path: "/register",
+    name: "RegisterComponent",
+    component: RegisterComponent,
   },
   {
-    path: '/forgot-password',
-    name: 'ForgotPassword',
-    component: ForgotPassword
+    path: "/forgot-password",
+    name: "ForgotPassword",
+    component: ForgotPassword,
   },
   {
-    path: '/:catchAll(.*)',
-    name: 'NotFound',
-    component: NotFound
+    path: "/home",
+    name: "HomeComponent",
+    component: HomeComponent,
   },
-]
+  {
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: NotFound,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 });
 
-router.beforeEach ((to, from, next) => {
+router.beforeEach((to, from, next) => {
   console.log(to, from);
-  next ();
-})
+  next();
+});
 
-export default router
+export default router;
